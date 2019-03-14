04cd139658bf3b3dd4bc3eff2927142a90357f66
Tyler Neely <t@jujit.su>
2019-03-14 17:52:08 +0100

    Make longrunning API available to library consumers

--- pbgen/src/lib.rs
+++ pbgen/src/lib.rs
@@ -6,32 +6,57 @@ extern crate tower_grpc;
 
 pub mod google {
     pub mod protobuf {
-        include!(concat!(env!("OUT_DIR"), "/google.protobuf.rs"));
+        include!(concat!(
+            env!("OUT_DIR"),
+            "/google.protobuf.rs"
+        ));
     }
 
     pub mod api {
-        include!(concat!(env!("OUT_DIR"), "/google.api.rs"));
+        include!(concat!(
+            env!("OUT_DIR"),
+            "/google.api.rs"
+        ));
+    }
+
+    pub mod longrunning {
+        include!(concat!(
+            env!("OUT_DIR"),
+            "/google.longrunning.rs"
+        ));
     }
 
     pub mod rpc {
-        include!(concat!(env!("OUT_DIR"), "/google.rpc.rs"));
+        include!(concat!(
+            env!("OUT_DIR"),
+            "/google.rpc.rs"
+        ));
     }
 
     pub mod bigtable {
         pub mod v2 {
-            include!(concat!(env!("OUT_DIR"), "/google.bigtable.v2.rs"));
+            include!(concat!(
+                env!("OUT_DIR"),
+                "/google.bigtable.v2.rs"
+            ));
         }
     }
 
     pub mod pubsub {
         pub mod v1 {
-            include!(concat!(env!("OUT_DIR"), "/google.pubsub.v1.rs"));
+            include!(concat!(
+                env!("OUT_DIR"),
+                "/google.pubsub.v1.rs"
+            ));
         }
     }
 
     pub mod spanner {
         pub mod v1 {
-            include!(concat!(env!("OUT_DIR"), "/google.spanner.v1.rs"));
+            include!(concat!(
+                env!("OUT_DIR"),
+                "/google.spanner.v1.rs"
+            ));
         }
     }
 }
